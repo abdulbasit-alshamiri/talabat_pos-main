18f442e067564a2386f4bf581c244f7adebecea9
abduhalshamiri <116202193+abduhalshamiri@users.noreply.github.com>
2024-10-31 01:14:01 +0300

    talabat_pos-main
    talabat_pos-main

---
+++ lib/utils/spaces.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+
+class SpacesApp {
+  static Widget spaceH_20 = SizedBox(
+    height: 20,
+  );
+
+  static Widget spaceH_10 = SizedBox(
+    height: 10,
+  );
+  static Widget spaceH_30 = SizedBox(
+    height: 30,
+  );
+  static Widget spaceH_40 = SizedBox(
+    height: 40,
+  );
+
+  static Widget spaceH_50 = SizedBox(
+    height: 50,
+  );
+  static Widget spaceW_10 = SizedBox(
+    width: 10,
+  );
+  static Widget spaceW_20 = SizedBox(
+    width: 20,
+  );
+}
